c62584b0a15f541ad54ef1913efc0c8832f21af2
KRABP <progmitelx@gmail.com>
2025-10-01 11:33:05 +0300

    Add files via upload

---
+++ ConsoleApp1.3.1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+class Program
+{
+    static void Main()
+    {
+        int numerator = GetNonNegativeInteger("Введите неотрицательный числитель: ");
+        int denominator = GetPositiveInteger("Введите положительный знаменатель: ");
+
+        Console.WriteLine($"\nИсходная дробь: {numerator}/{denominator}");
+
+        (int simplifiedNumerator, int simplifiedDenominator) = SimplifyFraction(numerator, denominator);
+
+        Console.WriteLine($"Сокращенная дробь: {simplifiedNumerator}/{simplifiedDenominator}");
+
+        if (simplifiedNumerator >= simplifiedDenominator && simplifiedDenominator != 1)
+        {
+            int wholePart = simplifiedNumerator / simplifiedDenominator;
+            int remainder = simplifiedNumerator % simplifiedDenominator;
+            if (remainder != 0)
+            {
+                Console.WriteLine($"В виде смешанного числа: {wholePart} {remainder}/{simplifiedDenominator}");
+            }
+            else
+            {
+                Console.WriteLine($"Целое число: {wholePart}");
+            }
+        }
+
+        double decimalValue = (double)numerator / denominator;
+        Console.WriteLine($"Десятичное представление: {decimalValue:F6}");
+    }
+
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    static (int numerator, int denominator) SimplifyFraction(int numerator, int denominator)
+    {
+        if (numerator == 0)
+            return (0, 1);
+
+        int gcd = GCD(numerator, denominator);
+
+        int simplifiedNumerator = numerator / gcd;
+        int simplifiedDenominator = denominator / gcd;
+
+        return (simplifiedNumerator, simplifiedDenominator);
+    }
+
+    static int GetNonNegativeInteger(string message)
+    {
+        while (true)
+        {
+            Console.Write(message);
+            if (int.TryParse(Console.ReadLine(), out int number) && number >= 0)
+                return number;
+            Console.WriteLine("Ошибка: введите неотрицательное целое чис-ло!");
+        }
+    }
+
+    static int GetPositiveInteger(string message)
+    {
+        while (true)
+        {
+            Console.Write(message);
+            if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+                return number;
+            Console.WriteLine("Ошибка: введите положительное целое число!");
+        }
+    }
+}
---
+++ ConsoleApp1.3.2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+class Program
+{
+    static void Main()
+    {
+        int targetSum = GetPositiveInteger("Введите максимальную сумму элементов: ");
+
+        var result = GenerateArrayWithSumLimit(targetSum);
+        int[] array = result.Item1;
+        int actualSum = result.Item2;
+        int count = result.Item3;
+
+        Console.WriteLine("\nРезультат:");
+        Console.WriteLine("Количество элементов: " + count);
+        Console.WriteLine("Сумма элементов: " + actualSum);
+        Console.WriteLine("Целевая сумма: " + targetSum);
+        Console.WriteLine("Разница: " + (targetSum - actualSum));
+
+        PrintArray(array, count);
+    }
+
+    static Tuple<int[], int, int> GenerateArrayWithSumLimit(int targetSum)
+    {
+        Random random = new Random();
+        List<int> elements = new List<int>();
+        int currentSum = 0;
+        int count = 0;
+
+        while (currentSum < targetSum)
+        {
+            int randomValue = random.Next(1, 10);
+            int potentialSum = currentSum + randomValue;
+
+            if (potentialSum > targetSum)
+                break;
+
+            elements.Add(randomValue);
+            currentSum = potentialSum;
+            count++;
+        }
+
+        return Tuple.Create(elements.ToArray(), currentSum, count);
+    }
+
+    static void PrintArray(int[] array, int count)
+    {
+        Console.WriteLine("\nМассив элементов:");
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write(array[i] + " ");
+            if ((i + 1) % 15 == 0)
+                Console.WriteLine();
+        }
+        Console.WriteLine();
+
+        var subArray = new List<int>();
+        for (int i = 0; i < count; i++)
+        {
+            subArray.Add(array[i]);
+        }
+        Console.Write("Сумма: " + string.Join(" + ", subArray) + " = " + subArray.Sum());
+        Console.WriteLine();
+    }
+
+
+
+    static int GetPositiveInteger(string message)
+    {
+        while (true)
+        {
+            Console.Write(message);
+            if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+                return number;
+            Console.WriteLine("Ошибка: введите положительное целое число!");
+        }
+    }
+}

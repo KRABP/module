e6e52ab79c3810e788d7ca78319fb33668a1203e
KRABP <progmitelx@gmail.com>
2025-10-01 11:30:02 +0300

    Add files via upload

---
+++ ConsoleApp1.1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Введите строку: ");
+        string input = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Console.WriteLine("Вы ввели пустую строку!");
+            return;
+        }
+        string reversed = ReverseString(input);
+        Console.WriteLine($"\nОригинал: {input}");
+        Console.WriteLine($"Реверс:   {reversed}");
+    }
+    static string ReverseString(string str)
+    {
+        char[] charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
